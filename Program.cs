681c383320a9a485a2c254ccb8d1dbc25726aef6
Elena San <gilbert2014@yandex.ru>
2024-02-11 18:47:00 +0300

    Создан метод фильтрации строк FilterStrings

--- Program.cs
+++ Program.cs
@@ -32,4 +32,29 @@ class Program
             }
         }    
 	}
+    static string[] FilterStrings(string[] input)
+    {
+        int count = 0;
+        foreach (string str in input)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] result = new string[count];
+        
+        int index = 0;
+        foreach (string str in input)
+        {
+            if (str.Length <= 3)
+            {
+                result[index] = str;
+                index++;
+            }
+        }
+
+        return result;
+    }
 }
